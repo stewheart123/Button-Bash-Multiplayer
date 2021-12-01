cc49e66a5ab239080402f114576dcbfd25f662b5
Stewart <44583600+stewheart123@users.noreply.github.com>
2021-12-01 17:24:42 +0000

    init

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    #region Network Objects and variables
+
+    [SerializeField] GameObject player;
+   
+    #endregion
+
+    #region Photon Network Overrides
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+        PhotonNetwork.Instantiate("Player", player.transform.position, player.transform.rotation);
+        if (PhotonNetwork.PlayerList.Length == 1)
+        {
+        
+          
+        }
+        if (PhotonNetwork.PlayerList.Length == 2)
+        {   
+           
+
+        }
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("Player has entered the room!");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Trying to connect to server");
+    }
+    
+    void Update()
+    {
+        
+    }
+    #endregion
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerInput : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public bool IsPlayerOne;
+    public int score;
+    public int hits;
+
+    public PlayerInput[] players;
+
+    private void Start()
+    {
+        players = FindObjectsOfType<PlayerInput>();
+
+
+        if (players.Length == 1)
+        {
+            IsPlayerOne = true;
+        }
+        else
+        {
+            IsPlayerOne = false;
+        }
+        
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(score);
+        }
+        else
+        {
+            this.score = (int)stream.ReceiveNext();
+        }
+        
+    }
+
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                score++;
+                Debug.Log("button pressed");
+            }
+        }
+        
+    }
+}
